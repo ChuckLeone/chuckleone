e6f0b0d924a994a4306182b8c7b6778c97c5a574
Chuck Leone <chuckleone@gmail.com>
2021-12-07 19:33:08 -0500

    installed react router 6
    updated to react router 6

---
+++ chuckleone/src/pages/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+
+const home = () => {
+    return (
+        <Grid container spacing={2}>
+            <Grid item sm={12}></Grid>
+            <Grid item sm={12}></Grid>
+        </Grid>
+    )
+};
+
+export default home;
