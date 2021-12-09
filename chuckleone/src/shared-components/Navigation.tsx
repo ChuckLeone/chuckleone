434e66cbd300985ce24bcad1cb681fa7d5da663c
Chuck Leone <chuckleone@gmail.com>
2021-12-09 14:17:50 -0500

    home page build out
    updates to appbar, icons and background slide, added pagination

--- chuckleone/src/shared-components/Navigation.tsx
+++ chuckleone/src/shared-components/Navigation.tsx
@@ -1,18 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Toolbar } from "@mui/material";
-import InstagramLogo from '../assets/Instagram_logo_2016.svg';
-import TwitterLogo from '../assets/twitter-logo.png';
+import { AppBar, Box, Button, IconButton, Toolbar } from "@mui/material";
+import { grey } from '@mui/material/colors'
+import MailIcon from '@mui/icons-material/Mail';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 const Navigation = () => {
+    const iconColor = grey['A100'];
+    
     return (
+        <AppBar color="transparent" sx={{boxShadow: 'none'}}>
         <Toolbar sx={{ marginLeft: '0', padding: '4px'}}>
             <Link to='/' style={{ textDecoration: 'none'}}><Button variant="text" style={{ color: '#fff'}}>Home</Button></Link>
             <Link to='/portfolio' style={{ textDecoration: 'none'}}><Button variant="text" style={{ color: '#fff'}}>Portfolio</Button></Link>
             <Link to='/music' style={{ textDecoration: 'none'}}><Button variant="text" style={{  color: '#fff'}}>Music</Button></Link>
-            <Link to="http://www.instagram.com/chv.ck.leone/" target="new"><img src={InstagramLogo} width="24px" height="24px" style={{ padding: '0 8px' }} /></Link>
-            <Link to="http://www.instagram.com/chv.ck.leone/" target="new"><img src={TwitterLogo} width="24px" height="24px" /></Link>
+            <Link to='/monstersandmachines' style={{ textDecoration: 'none'}}><Button variant="text" style={{  color: '#fff'}}>Monsters and Machines</Button></Link>
+            <Box sx={{ flexGrow: 1 }} />
+            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                <IconButton size="large" aria-label="email link" color="inherit">
+                    <MailIcon />
+                </IconButton>
+                <IconButton size="large" aria-label="instagram link" color="inherit">
+                <a href="http://www.instagram.com/chv.ck.leone/" target="new">
+                    <InstagramIcon sx={{ color: iconColor }}/>
+                </a>
+                </IconButton>
+                <IconButton size="large" aria-label="twitter link" color="inherit">
+                    <a href="http://www.twitter.com/chuckleone/" target="new">
+                        <TwitterIcon sx={{ color: iconColor }} />
+                    </a>
+                </IconButton>
+            </Box>
         </Toolbar>
+        </AppBar>
     )
 };
 
--- chuckleone/src/shared-components/SiteBanner.tsx
+++ chuckleone/src/shared-components/SiteBanner.tsx
@@ -1,29 +1,47 @@
 import React from "react";
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Grid, IconButton, Pagination, PaginationItem, Stack, Typography } from '@mui/material';
+import { grey } from '@mui/material/colors'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import VaporCanyon from '../assets/vr-vapor-canyon.jpg';
 import Navigation from "./Navigation";
 
 const SiteBanner = () => {
+    const liteColor = grey['A100'];
+
     return(
-        <div style={{ backgroundImage: `url(${VaporCanyon})`, backgroundRepeat: 'no-repeat', minHeight: '100vh', width: '100%', backgroundColor: '#000', backgroundPosition: 'center'}}>
-            <Box>
-                <Box style={{ backgroundColor: 'rgba(0,0,0,0.6', color: 'white', minHeight: '20vh', width: '100%', padding: '8px'}}>
-            <Grid container>
-                <Grid item sm={12} sx={{ marginLeft: '32px', marginTop: '20px'}}>
-                    <Typography variant="h4" component="h1" >CHV.CK LEONE</Typography></Grid>
-                <Grid item sm={12}>
-                    <Navigation />
-                </Grid>
-            </Grid>
-            </Box>
-            <Box>
-            <Grid container sx={{ marginLeft: '40px', marginTop: '20px'}}>
-                <Grid item sm={12}>
-                    Content here
-                </Grid>
-            </Grid>
-            </Box>
-            </Box>
+        <div style={{ backgroundImage: `url(${VaporCanyon})`, backgroundRepeat: 'no-repeat', minHeight: '100vh', width: '100%', backgroundColor: '#000', backgroundPosition: 'center', backgroundSize: 'cover'}}>
+            <div style={{ backgroundColor: 'rgba(0,0,0,0.3', color: 'white', minHeight: '100vh', width: '100%', padding: '8px'}}>
+                <Box sx={{ minHeight: '60vh'}}>
+                    <Grid container>   
+                        <Grid item sm={12}>
+                            <Navigation />
+                        </Grid>
+                    </Grid>  
+                </Box>
+                <Box>
+                    <Grid container spacing={2}>
+                        <Grid item sm={12} sx={{ marginLeft: '20px'}}>
+                            <Typography variant="h1" component="h1" >CHV.CK LEONE</Typography>
+                            <Typography variant="h6" component="h2">Developer | Designer | Artist</Typography>
+                        </Grid>
+                        <Grid item sm={12} sx={{textAlign: 'center', marginRight: '40px', marginTop: '180px'}}>
+                            <Typography variant="h5" sx={{color: 'rgba(255,255,255,0.6)'}}>Vapor Canyon</Typography>
+                            <Typography variant="body1" sx={{color: 'rgba(255,255,255,0.6)'}}>Custom Unity 3D shader</Typography>
+                        </Grid>
+                        <Grid item sm={12} sx={{ textAlign: 'center'}}>
+                            <Stack spacing={2}>
+                                <Pagination count={3} color="secondary" renderItem={(item) => (
+                                    <PaginationItem sx={{color: '#fff'}}
+                                    components={{ previous: ArrowBackIosIcon, next: ArrowForwardIosIcon }}
+                                    {...item} 
+                                    />)} />
+                            </Stack>
+                        </Grid>
+                    </Grid>
+                </Box>
+                
+            </div>
         </div>
     );
 };
